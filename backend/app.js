7376f86aff88926ee0da4c362abb1f07b5a6e3c0
Axel <axel.burguiere76@gmail.com>
2022-01-18 16:28:28 +0100

    avancement final

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,35 @@
-// Variables d'environnement
 require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const sauceRoutes = require('./routes/Sauce')
 const userRoutes = require('./routes/User');
 const path = require('path');
+const helmet = require("helmet");
 
-// Connexion à MongoDB
-mongoose.connect('mongodb+srv://Desert76:Eb447790^^@cluster0.g81ga.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.g81ga.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    .catch(() => console.log('Connexion à MongoDB échouée...'));
 
-// Création de l'application express
 const app = express();
+app.use(helmet());
 
-// Gestion des CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-
-// Parsing body en JSON
 app.use(bodyParser.json());
 
-// Routes
+app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
-// Exportation de l'appli
 module.exports = app;
